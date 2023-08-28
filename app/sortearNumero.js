b84a9b459c47b0619fe80751fb89cab41589123c
Paula Gaspar <139185489+HanePaula@users.noreply.github.com>
2023-08-28 20:14:22 -0300

    envio-atualizado
    Atualizei a aplicação.

--- app/sortearNumero.js
+++ app/sortearNumero.js
@@ -1,12 +1,10 @@
-const menorValor = 1;
+const menorValor = 10;
 const maiorValor = 1000;
 const numeroSecreto = gerarNumeroAleatorio();
 
 function gerarNumeroAleatorio(){
     return parseInt(Math.random() * maiorValor + 1);
 }
-
-console.log("Número secreto:", numeroSecreto);
 
 const elementoMenorValor = document.getElementById('menor-valor');
 elementoMenorValor.innerHTML = menorValor;
--- app/validacao.js
+++ app/validacao.js
@@ -1,15 +1,23 @@
 function verificaChute(chute){
     const numero = +chute;
 
-    if(chuteInvalido(numero)){
+    if(chute === 'game over'){
+        document.body.innerHTML = `<h1>GAME OVER</h1>
+        <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>`
+        document.body.style.backgroundColor = "var(--background-gameover)"
+        document.body.style.color = "var(--background-color)"
+        document.body.style.fontWeight = "900"
+        document.getElementById('jogar-novamente').style.backgroundColor = "var(--background-color)"
+    }
+    else if(chuteInvalido(numero)){
         elementoChute.innerHTML += '<div>Valor inválido</div>';
         return
     }
-    if(numeroForaDosLimites(numero)){
+    else if(numeroForaDosLimites(numero)){
         elementoChute.innerHTML += `<div>Valor inválido: Fale um número entre ${menorValor} e ${maiorValor}</div>`
         return
     }
-    if(numero === numeroSecreto){
+    else if(numero === numeroSecreto){
         document.body.innerHTML = `<h2>Você acertou!</h2>
         <h3>O número secreto era ${numeroSecreto}</h3>
         <button id="jogar-novamente" class="btn-jogar">Jogar novamente</button>
